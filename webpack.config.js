1eb8cbcc7dc0654ca4aa08bd53c472b2de18f3f6
Pedro Ferreira <plferreira4@gmail.com>
2020-04-10 16:42:39 +0200

    Update the build script.
    - Clean the 'dist' directory before building.
    - Copy the necessary files (images, sounds, etc).
    - When running the dev server, open the page automatically.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path = require("path");
+const Path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = function (env, argv) {
     const mode = argv.mode;
@@ -7,7 +9,7 @@ module.exports = function (env, argv) {
         entry: "./source/main.ts",
         output: {
             filename: "bundle.js",
-            path: path.resolve(__dirname, "dist"),
+            path: Path.resolve(__dirname, "dist"),
         },
         module: {
             rules: [
@@ -22,5 +24,34 @@ module.exports = function (env, argv) {
         resolve: {
             extensions: [".tsx", ".ts", ".js"],
         },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new CopyWebpackPlugin([
+                {
+                    from: "index.html",
+                    to: "index.html",
+                },
+                {
+                    from: "css/",
+                    to: "css/",
+                },
+                {
+                    from: "libraries/",
+                    to: "libraries/",
+                },
+                {
+                    from: "sound/",
+                    to: "sound/",
+                },
+                {
+                    from: "maps/",
+                    to: "maps/",
+                },
+                {
+                    from: "images/*.png",
+                    to: "./",
+                },
+            ]),
+        ],
     };
 };
